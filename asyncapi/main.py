90d2667c22b97b783ed91a1fc945e75f7bf93ec9
Тимур Зайнуллин <tvzaynullin@gmail.com>
2021-10-17 15:27:43 +0300

    Refactoring  asyncapi 3

--- asyncapi/main.py
+++ asyncapi/main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     uvicorn.run(
         'app:app',
         host='0.0.0.0',
-        port=8000,
+        port=8001,
         log_level=logging.DEBUG,
         reload=True
     )
